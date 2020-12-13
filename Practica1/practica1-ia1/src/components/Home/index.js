e195a3f9c824674265175c4689090a3bfc595ce3
Oscar Rene Cuellar Mancilla <usackoka@gmail.com>
2020-12-13 17:55:26 -0600

    Frontend base

---
+++ Practica1/practica1-ia1/src/components/Home/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container, Navbar } from "react-bootstrap";
+
+const Home = () => {
+  return (
+    <Container>
+      <Navbar expand="lg" variant="light" bg="light">
+        <Navbar.Brand href="#">Navbar</Navbar.Brand>
+      </Navbar>
+    </Container>
+  );
+};
+
+export default Home;
